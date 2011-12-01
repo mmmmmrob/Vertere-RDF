5e338ab885bf5fcd17523ec600d34bd68b97a87b
mmmmmrob <rob.styles@dynamicorange.com>
2011-12-01 23:12:49 +0000

    changed output to triples for bulk and added an ignore for tmp.rdf.nt

--- vertere_mapper.php
+++ vertere_mapper.php
@@ -38,6 +38,6 @@ for ($i = 0; $i < $header_rows; $i++) { $reader->next_record(); }
 //Create graph from records
 while ($record = $reader->next_record()) {
 	$output_graph = $vertere->convert_array_to_graph($record);
-	echo $output_graph->to_turtle();
+	echo $output_graph->to_ntriples();
 	$output_graph->remove_all_triples();
 }
