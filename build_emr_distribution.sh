26782c75127158821ed4d7572c6fc131965ae33b
MalcyL <malcolm@landonsonline.me.uk>
2012-03-12 15:07:47 +0000

    add script to build distribution used by amazon emr and vertimus.

---
+++ build_emr_distribution.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+rm -rf dist;
+mkdir dist;
+
+cp *.php dist;
+cp -R inc dist;
+cp -R src dist;
+
+mkdir dist/lib;
+cp lib/simplegraph_functions.txt dist/lib;
+cd dist/lib;
+svn checkout http://moriarty.googlecode.com/svn/trunk/ moriarty ;
+git clone https://github.com/semsol/arc2.git arc ;
+cd ../..;
+
+cd dist;
+tar -czvf vertere_emr_distribution.tar.gz *;
+cd ..;
