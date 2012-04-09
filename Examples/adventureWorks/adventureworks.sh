f888e469558010e59912271d8fd273d06d204dd8
Knud Möller <dunken69@gmail.com>
2012-04-09 21:00:39 +0100

    conversion finished, tidying up

--- Examples/adventureWorks/adventureworks.sh
+++ Examples/adventureWorks/adventureworks.sh
@@ -1,65 +1,66 @@
 #!/bin/bash
 
-PROJECT_FOLDER="adventureWorks"
+TEMPLATE_FOLDER="templates/"
+VERTERE_FOLDER="../../"
 
-
-
-cat /dev/null > $PROJECT_FOLDER/output_data/full.rdf.nt
+cat /dev/null > output_data/full.rdf.nt
 
 echo Starting with turtle files in hand-written rdf
-for file in $(ls $PROJECT_FOLDER/handwritten/*.ttl)
+for file in $(ls handwritten/*.ttl)
 do
-	rapper -i turtle -o ntriples "$file" >> $PROJECT_FOLDER/output_data/full.rdf.nt
+	rapper -i turtle -o ntriples "$file" >> output_data/full.rdf.nt
 done
 
 echo Validating spec files
-for file in $(ls $PROJECT_FOLDER/*.spec.ttl)
+for file in $(ls templates/*.spec.ttl)
 do
 	rapper -i turtle -c "$file"
 done
 
+echo Describing customers
+cat AdventureWorks_2008R2_LT/Customer.tsv | $VERTERE_FOLDER/vertere_mapper.php $TEMPLATE_FOLDER/customer.tsv.spec.ttl >> output_data/full.rdf.nt
 
-# echo Describing customers
-# cat $PROJECT_FOLDER/AdventureWorks_2008R2_LT/Customer.tsv | ../vertere_mapper.php $PROJECT_FOLDER/customer.tsv.spec.ttl >> $PROJECT_FOLDER/output_data/full.rdf.nt
-# 
-# echo Describing addresses
-# cat $PROJECT_FOLDER/AdventureWorks_2008R2_LT/Address.csv | ../vertere_mapper.php $PROJECT_FOLDER/address.csv.spec.ttl >> $PROJECT_FOLDER/output_data/full.rdf.nt
-# 
-# echo Linking customers with addresses
-# cat $PROJECT_FOLDER/AdventureWorks_2008R2_LT/CustomerAddress.csv | ../vertere_mapper.php $PROJECT_FOLDER/customer_address.csv.spec.ttl >> $PROJECT_FOLDER/output_data/full.rdf.nt
-# 
-# echo Describing product categories
-# cat $PROJECT_FOLDER/AdventureWorks_2008R2_LT/ProductCategory.csv | ../vertere_mapper.php $PROJECT_FOLDER/product_category.csv.spec.ttl >> $PROJECT_FOLDER/output_data/full.rdf.nt
-# 
-# echo Describing products
-# cat $PROJECT_FOLDER/AdventureWorks_2008R2_LT/Product.tsv | ../vertere_mapper.php $PROJECT_FOLDER/product.csv.spec.ttl >> $PROJECT_FOLDER/output_data/full.rdf.nt
+echo Describing addresses
+cat AdventureWorks_2008R2_LT/Address.csv | $VERTERE_FOLDER/vertere_mapper.php $TEMPLATE_FOLDER/address.csv.spec.ttl >> output_data/full.rdf.nt
+
+echo Linking customers with addresses
+cat AdventureWorks_2008R2_LT/CustomerAddress.csv | $VERTERE_FOLDER/vertere_mapper.php $TEMPLATE_FOLDER/customer_address.csv.spec.ttl >> output_data/full.rdf.nt
+
+echo Describing product categories
+cat AdventureWorks_2008R2_LT/ProductCategory.csv | $VERTERE_FOLDER/vertere_mapper.php $TEMPLATE_FOLDER/product_category.csv.spec.ttl >> output_data/full.rdf.nt
+
+echo Describing products
+cat AdventureWorks_2008R2_LT/Product.tsv | $VERTERE_FOLDER/vertere_mapper.php $TEMPLATE_FOLDER/product.csv.spec.ttl >> output_data/full.rdf.nt
 
 echo Describing orders
-cat $PROJECT_FOLDER/AdventureWorks_2008R2_LT/SalesOrderHeader.csv | ../vertere_mapper.php $PROJECT_FOLDER/sales_order_header.csv.spec.ttl >> $PROJECT_FOLDER/output_data/full.rdf.nt
+cat AdventureWorks_2008R2_LT/SalesOrderHeader.csv | $VERTERE_FOLDER/vertere_mapper.php $TEMPLATE_FOLDER/sales_order_header.csv.spec.ttl >> output_data/full.rdf.nt
+
+echo Describing order details
+cat AdventureWorks_2008R2_LT/SalesOrderDetail.csv | $VERTERE_FOLDER/vertere_mapper.php $TEMPLATE_FOLDER/sales_order_detail.csv.spec.ttl >> output_data/full.rdf.nt
 
 echo Sorting and de-duping descriptions
-sort -u $PROJECT_FOLDER/output_data/full.rdf.nt > $PROJECT_FOLDER/output_data/adventureworks.rdf.nt
+sort -u output_data/full.rdf.nt > output_data/adventureworks.rdf.nt
 
-# rm $PROJECT_FOLDER/output_data/full.rdf.nt
+rm output_data/full.rdf.nt
 
 #echo De-duping and extending descriptions
 #cat ourairports.com/output_data/sorted.rdf.nt | ../vertere_reducer.php > ourairports.com/output_data/ourairports.rdf.nt
 
 echo Listing properties used
-cat $PROJECT_FOLDER/output_data/adventureworks.rdf.nt | awk '{ print $2 }' | sort -u > $PROJECT_FOLDER/output_data/adventureworks.properties_used.txt
+cat output_data/adventureworks.rdf.nt | awk '{ print $2 }' | sort -u > output_data/adventureworks.properties_used.txt
 
 echo Listing classes used
-cat $PROJECT_FOLDER/output_data/adventureworks.rdf.nt | awk '{ print $2 " " $3 }' | grep "^<http://www.w3.org/1999/02/22-rdf-syntax-ns#type> " | awk '{ print $2 }' | sort -u > $PROJECT_FOLDER/output_data/adventureworks.classes_used.txt
+cat output_data/adventureworks.rdf.nt | awk '{ print $2 " " $3 }' | grep "^<http://www.w3.org/1999/02/22-rdf-syntax-ns#type> " | awk '{ print $2 }' | sort -u > output_data/adventureworks.classes_used.txt
 
 echo Converting descriptions to turtle
-rapper -i ntriples -o turtle $PROJECT_FOLDER/output_data/adventureworks.rdf.nt \
+rapper -i ntriples -o turtle output_data/adventureworks.rdf.nt \
 	-f 'xmlns:foaf="http://xmlns.com/foaf/0.1/"' \
 	-f 'xmlns:schema="http://schema.org/"' \
 	-f 'xmlns:vcard="http://www.w3.org/2006/vcard/ns#"' \
-	-f 'xmlns:scv="http://data.kasabi.com/dataset/adventure_works/schema/"' \
+	-f 'xmlns:adv="http://data.kasabi.com/dataset/adventure_works/schema/"' \
 	-f 'xmlns:xsd="http://www.w3.org/2001/XMLSchema#"' \
 	-f 'xmlns:gr="http://purl.org/goodrelations/v1#"' \
-	> $PROJECT_FOLDER/output_data/adventureworks.rdf.ttl
+	> output_data/adventureworks.rdf.ttl
 
 # echo Converting descriptions to turtle
 # rapper -i ntriples -o turtle -f'xmlns:conv="http://example.com/schema/data_conversion#"' -f'xmlns:bibo="http://example.com/bibo#"' -f'xmlns:fly="http://data.kasabi.com/dataset/airports/schema/"' -f'xmlns:foaf="http://xmlns.com/foaf/0.1/"' -f'xmlns:geo="http://www.w3.org/2003/01/geo/wgs84_pos#"' -f'xmlns:georss="http://www.georss.org/georss/"' -f'xmlns:owl="http://www.w3.org/2002/07/owl#"' -f'xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#"' -f'xmlns:rdfs="http://www.w3.org/2000/01/rdf-schema#"' -f'xmlns:spacerel="http://data.ordnancesurvey.co.uk/ontology/spatialrelations/"' -f'xmlns:xsd="http://www.w3.org/2001/XMLSchema#"' ourairports.com/output_data/ourairports.rdf.nt > ourairports.com/output_data/ourairports.rdf.ttl
@@ -67,3 +68,8 @@ rapper -i ntriples -o turtle $PROJECT_FOLDER/output_data/adventureworks.rdf.nt \
 # echo Converting descriptions to rdfxml
 # rapper -i ntriples -o rdfxml-abbrev -f'xmlns:conv="http://example.com/schema/data_conversion#"' -f'xmlns:bibo="http://example.com/bibo#"' -f'xmlns:fly="http://data.kasabi.com/dataset/airports/schema/"' -f'xmlns:foaf="http://xmlns.com/foaf/0.1/"' -f'xmlns:geo="http://www.w3.org/2003/01/geo/wgs84_pos#"' -f'xmlns:georss="http://www.georss.org/georss/"' -f'xmlns:owl="http://www.w3.org/2002/07/owl#"' -f'xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#"' -f'xmlns:rdfs="http://www.w3.org/2000/01/rdf-schema#"' -f'xmlns:spacerel="http://data.ordnancesurvey.co.uk/ontology/spatialrelations/"' -f'xmlns:xsd="http://www.w3.org/2001/XMLSchema#"' ourairports.com/output_data/ourairports.rdf.nt > ourairports.com/output_data/ourairports.rdf.xml
 # 
+
+echo Preparing for upload
+rm upload/*
+split -l 9000 output_data/adventureworks.rdf.nt adventureworks.
+mv adventureworks.a* upload
